3b1f62f38b01ffa6eada3118bcd3fb7c536d843e
borismg04 <borismg04@gmail.com>
2022-06-13 11:50:57 -0500

    fundamentos variables

--- JavaScript/01.Variables.js
+++ JavaScript/01.Variables.js
@@ -1,2 +1,11 @@
-// Variables
-console.log("Hola mundo"); 
+// Variables -var let const
+
+let cliente = "Boris";
+let precioDescuento = 220;
+
+//Reasignar valor
+cliente = 20;
+
+console.log("Cliente: " + cliente);
+console.log("Precio con descuento: " + precioDescuento);
+console.log(precioDescuento);
