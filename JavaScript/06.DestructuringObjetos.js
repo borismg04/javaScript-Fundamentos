378e993a7f6ee12a551dd54de8c4995d7d091e39
borismg04 <borismg04@gmail.com>
2022-06-29 11:09:09 -0500

    destructuring

--- JavaScript/06.DestructuringObjetos.js
+++ JavaScript/06.DestructuringObjetos.js
@@ -5,3 +5,14 @@ const producto= {
   precio: 1000,
   disponible: true,
 }
+
+const cliente = {
+  nombre: 'Boris',
+  premium: true,
+}
+
+const { nombre , precio , disponible } = producto;
+const { nombre: nombreCliente , premium } = cliente;
+
+console.log(nombre , precio , disponible);
+console.log(nombreCliente , premium);
