1bb68ed2b109e2966c0dcc2ea5c4684cebd0c28f
borismg04 <borismg04@gmail.com>
2022-06-29 10:19:03 -0500

    manipulacion de objetos

--- JavaScript/05.ManipulacionObjetos.js
+++ JavaScript/05.ManipulacionObjetos.js
@@ -6,7 +6,21 @@ const producto= {
   disponible: true,
 }
 
+//Para no modificar el objeto original
+// Object.freeze(producto); Freeze nos permite bloquear los cambios en el objeto original , no permite añadir ni eliminar propiedades
+
+// Para modificar el objeto original pero no se permite eliminar o agregar propiedades
+// Object.seal(producto); Seal nos permite modificar las propiedades del objeto pero no eliminar ni agregar
+
+//Reescribir el objeto o un valor de un objeto
 producto.nombre = 'Monitor Curvo';
+
+//Si no existe una propiedad, se crea
+producto.imagen = 'imagen.jpg';
+
+//Eliminar una propiedad
+delete producto.disponible;
 
 console.log(producto);
 console.table(producto);
+
