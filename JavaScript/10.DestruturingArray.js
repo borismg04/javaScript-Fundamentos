d0100437cf6f3e5142d8d2cfa52f3aa6daced64e
borismg04 <borismg04@gmail.com>
2022-06-30 10:13:49 -0500

    funciones declaradas

---
+++ JavaScript/10.DestruturingArray.js
@@ -0,0 +1,11 @@
+// Destructuring Array
+const tecnologias = ['HTML','CSS','JavaScript','React','NodeJS'];
+
+const [var1,var2,var3,var4,var5]=tecnologias;
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(var4);
+console.log(var5);
+
