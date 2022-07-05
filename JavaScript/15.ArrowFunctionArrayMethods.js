9a394fd0d1f164d2930b37e28ddcfcb008cc7ba4
borismg04 <borismg04@gmail.com>
2022-07-05 10:27:08 -0500

    arrow function

---
+++ JavaScript/15.ArrowFunctionArrayMethods.js
@@ -0,0 +1,19 @@
+// Funciones - Arrow Function y Array Methods
+const tecnologias = ['HTML','CSS','JavaScript','React','NodeJS'];
+
+//Ejemplo 1 = Arrow Function (tech =>)
+const newArray = tecnologias.map( (tech)=> {
+  if(tech === 'JavaScript'){
+    return tech = 'REEMPLAZO';
+  } else {
+    return tech;
+  }
+})
+//Ejemplo 2 = Arrow Function (tech => )
+const newArray2 = tecnologias.filter( (tech) =>{
+  return tech === 'React';//Sola mente me imprime React
+})
+
+
+console.log('newArray:', newArray)
+console.log('newArray2:', newArray2)
