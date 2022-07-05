62ac93e265d572653673c809ec94d782f047752c
borismg04 <borismg04@gmail.com>
2022-07-05 09:54:33 -0500

    funciones expresadas

--- JavaScript/13.FunctionExpression.js
+++ JavaScript/13.FunctionExpression.js
@@ -1,2 +1,13 @@
 // Funciones - Function Expression
 
+// function sumar(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+//Esta es la forma de definir una funcion
+const sumar = function() {
+  console.log(2 + 2);
+}
+
+sumar();
+
