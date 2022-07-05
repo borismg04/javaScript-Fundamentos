096f3a53a0b780f9da0e030b4cc69cb3b9aa83a0
borismg04 <borismg04@gmail.com>
2022-07-05 12:40:53 -0500

    condiocionales comparadores

---
+++ JavaScript/17.CondicionalesIf.js
@@ -0,0 +1,26 @@
+// Condicionales IF
+
+const disponible = 4000;
+const retirar = 100;
+
+if (disponible>=retirar){
+  //Se cumple la condicion y se ejecuta el bloque de codigo
+    console.log("Retiro exitoso");
+} else {
+  //No se cumple la condicion y no se ejecuta el bloque de codigo
+    console.log("No hay suficiente dinero");
+}
+
+/**
+ * > mayor que
+ * < menor que
+ * >= mayor o igual que
+ * <= menor o igual que
+ * == igual que
+ * != diferente que
+ * === identico que
+ * !== no identico que
+ * && and
+ * || or
+ * ! not
+ */
