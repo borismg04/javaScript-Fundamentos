951e51fb1f87f18968e34566b820e731693521da
borismg04 <borismg04@gmail.com>
2022-07-07 17:27:18 -0500

    finish

--- JavaScript/32.TemplatesString.js
+++ JavaScript/32.TemplatesString.js
@@ -11,3 +11,5 @@ function textoFuncion() {
 console.log(producto, precio, marca); //Tablet de 10 pulgadas 400 Samsung
 console.log(`El producto ${producto} cuesta ${precio} y es marca ${marca}`); //El producto Tablet de 10 pulgadas cuesta 400 y es marca Samsung
 console.log(`El producto ${producto} cuesta ${precio} y es marca ${marca} , ${textoFuncion()}`); //El producto Tablet de 10 pulgadas cuesta 400 y es marca Samsung Esto proviene de la funcion
+
+"Fundamento de JavaScript"
