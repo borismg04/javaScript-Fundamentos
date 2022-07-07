e53c484d6403f538dfadd26d4f85e32dc9c52433
borismg04 <borismg04@gmail.com>
2022-07-07 16:51:21 -0500

    async await

---
+++ JavaScript/28.1-funciones.js
@@ -0,0 +1,17 @@
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+
+function restar(num1, num2) {
+    return num1 - num2;
+}
+
+// Se puede exportar varias funciones
+export {
+    sumar,
+    restar
+}
+
+//Solo se puede exportar una funcion
+// export default sumar;
+// export default restar;
