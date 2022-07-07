614ccea7cd2cdfaaa78db90e75828cdef08a018e
borismg04 <borismg04@gmail.com>
2022-07-07 17:30:32 -0500

    templates string

--- JavaScript/32.TemplatesString.js
+++ JavaScript/32.TemplatesString.js
@@ -1,5 +1,4 @@
 // Templates String
-
 const producto = "Tablet de 10 pulgadas";
 const precio = 400;
 const marca = "Samsung";
@@ -11,5 +10,3 @@ function textoFuncion() {
 console.log(producto, precio, marca); //Tablet de 10 pulgadas 400 Samsung
 console.log(`El producto ${producto} cuesta ${precio} y es marca ${marca}`); //El producto Tablet de 10 pulgadas cuesta 400 y es marca Samsung
 console.log(`El producto ${producto} cuesta ${precio} y es marca ${marca} , ${textoFuncion()}`); //El producto Tablet de 10 pulgadas cuesta 400 y es marca Samsung Esto proviene de la funcion
-
-"Fundamento de JavaScript"
